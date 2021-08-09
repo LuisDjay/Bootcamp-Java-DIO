0b5abf40fad59d9a41b424483c37c83d54aae2d4
Luís Roberto <luisldjsilva@gmail.com>
2021-08-09 14:06:23 -0300

    testes

--- exemplo-maven/src/main/java/br/com/globallabs/exemplomaven/ExemploMavenApplication.java
+++ exemplo-maven/src/main/java/br/com/globallabs/exemplomaven/ExemploMavenApplication.java
@@ -1,4 +1,4 @@
-package br.com.globalabs.exemplomaven;
+package br.com.globallabs.exemplomaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExemploMavenApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
+System.out.println("Hello World!");
 		SpringApplication.run(ExemploMavenApplication.class, args);
 	}
 
